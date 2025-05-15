4caca2d76962d7d573c65b2c2e0d9b7adf5a306b
Alice-C-C <alicechimim@gmail.com>
2025-05-15 20:35:32 -0300

    mong

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,6 @@ const connectDB = async () => {
   try {
     await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.CLUSTER_ADDRESS}/${process.env.DB_NAME}`, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
     console.log('Conectado ao Mongoose');
   } catch (err) {
